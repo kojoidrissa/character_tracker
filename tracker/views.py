487e81d5ca8755f4ccf363db7989bfae8f8e69eb
Kojo Idrissa <kojo.idrissa@gmail.com>
2025-03-20 16:52:34 -0500

    ListView, UGLY template, new url

--- tracker/views.py
+++ tracker/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.views.generic import ListView
+from tracker.models import Character
+
+
+class CharacterListView(ListView):
+    model = Character
+    # template_name = "templates/character_list.html"
+    context_object_name = "character_list"
